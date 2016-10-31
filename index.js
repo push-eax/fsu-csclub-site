b0f0c0dfbc071f687a4769956520ebfc5c88b172
Jim Read <jimmydean886@hotmail.com>
2016-10-30 21:34:59 -0400

    Shifted menu button animations, animate menu too
    Menu now animates opening and closing. To prevent visual oddities,
    the scroll bar has been removed. This interface is therefore
    declared unsuitable for small displays.
    
    The menu button also animates in a more reasonable manner. Instead
    of rotating on mouseover like its original design, it animates to
    point in the direction the menu will move.

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ function onmenubout(){
 var menuopen = false;
 function openmenu(){
 	if(menuopen == false){
-		document.getElementById("menu").style.display='block';
 		document.getElementById("menubackground").style.display='block';
+		document.getElementById("menu").style.height='80%';
 		menuopen = true
+		document.getElementById("menubutton").style.transform='rotate(180deg)';
 	}
 	else{
-		document.getElementById("menu").style.display='none';
 		document.getElementById("menubackground").style.display='none';
 		menuopen = false
+		document.getElementById("menu").style.height='0px';
+		document.getElementById("menubutton").style.transform='rotate(0deg)';
 	}
 }
