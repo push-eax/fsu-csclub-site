9649c07661de32a8cd5aff89d2342aed1250be2b
Jim Read <jimmydean886@hotmail.com>
2016-10-30 21:22:50 -0400

    Added main menu
    Main menu with background for disabling open menu implemented.
    No animations yet, but that will come.

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ function onmenubover(){
 function onmenubout(){
 	document.getElementById("menubutton").src="beta-start-button.png";
 }
+var menuopen = false;
+function openmenu(){
+	if(menuopen == false){
+		document.getElementById("menu").style.display='block';
+		document.getElementById("menubackground").style.display='block';
+		menuopen = true
+	}
+	else{
+		document.getElementById("menu").style.display='none';
+		document.getElementById("menubackground").style.display='none';
+		menuopen = false
+	}
+}
