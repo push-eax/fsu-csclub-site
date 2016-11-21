547c1d4e01e5ecd15eb74743bf8e0a5315eff96a
Jim Read <jimmydean886@hotmail.com>
2016-11-20 19:56:59 -0500

    Window moving now works 100%
    Added window moving support, complete with the ability to click-and-
    drag windows. The contents do not update during move as of yet, but
    the previous limitation requiring the mouse to remain within the
    window has been removed.
    
    This version logs a crap load to the console, and isn't final. Once
    the window model is complete, it should become less verbose.

--- widgetTools.js
+++ widgetTools.js
@@ -1,4 +1,38 @@
 /*
+Global variables
+
+positiondownx
+	position of the mouse (x-coord) on window move click
+	
+positiondowny
+	position of the mouse (y-coord) on window move click
+
+positionupx
+	position of the mouse (x-coord) on mouseup
+
+positionupy
+	position of the mouse (y-coord) on mouseup
+
+wtomove
+	global variable representing a window object
+	used to define which window should be moved when a window
+	needs moving. Otherwise, not really used (right now)
+
+justmoved
+	true on mousedown before mouseup when moving a window.
+	A more accurate name might be currentlymovingwindow, but
+	that's pretty verbose and I'm too lazy to either come up 
+	with a better name or change this one now that it works.
+*/
+var positiondownx;
+var positiondowny;
+var positionupx;
+var positionupy;
+var wtomove;
+var justmoved = false;
+
+
+/*
 movewindow(currentwindow, increasex, increasy)
 
 Used to move a window by a certain offset X and Y.
@@ -27,24 +61,81 @@ function movewindow(currentwindow, increasex, increasey){
 	currentwindow.style.top = newy + "px";
 }
 
-var positiondownx;
-var positiondowny;
-var positionupx;
-var positionupy;
+/*
+clickdown(ev, element)
 
-function clickdown(ev){
+Used as an event handling function for when someone clicks
+the title bar of a window expecting to be able to move it.
+
+Arguments:
+	ev
+		event object.
+		Supplied by the event itself, should not be manual.
+		Assumes mouse event.
+	
+	element
+		element object, usually a div but not picky
+		Supplied by the addWindowListeners(element) function,
+		this defines what should be moved. It is currently
+		designed around the assumption that the left and top
+		style parameters work, meaning a position mode is set
+		other than the default. Works with class=window.
+*/
+function clickdown(ev,element){
 	positiondownx = ev.pageX;
 	positiondowny = ev.pageY;
+	wtomove = element;
+	justmoved = true;
 	console.log ("Click registered with coords: ", positiondownx, positiondowny);
 }
-function clickup(ev,element){
+
+/*
+clickup(ev)
+
+Used as another event handler, for when someone releases the
+mouse. It picks up whether or not there are any drawn windows,
+and should be used for window management tasks. At the time of
+writing it checks to see if the window should be moved, and
+if so, it moves the window. Otherwise, it does nothing substantial.
+
+Arguments:
+	ev
+		event object.
+		Supplied by the event itself, should not be manual.
+		Assumes mouse event.
+*/
+function clickup(ev){
 	positionupx = ev.pageX;
 	positionupy = ev.pageY;
-	movewindow(element, positionupx - positiondownx, positionupy - positiondowny);
+	if(justmoved == true){
+		movewindow(wtomove, positionupx - positiondownx, positionupy - positiondowny);
+		justmoved = false;
+	}
 	console.log("mouseup at ", positionupx, positionupy, positiondownx, positiondowny);
 }
 
+/*
+addWindowListeners(currentwindow)
+
+Adds event handlers to the current window object.
+
+Arguments:
+	currentwindow
+		current window element. Used to identify the window
+		later, should be a JS DOM element. When in doubt, this
+		can be gotten with document.getElementById()
+*/
 function addWindowListeners(currentwindow){
-	currentwindow.onmousedown = function(event){clickdown(event)};
-	currentwindow.onmouseup   = function(event){clickup  (event, currentwindow)};
+	currentwindow.onmousedown = function(event){clickdown(event, currentwindow)};
 }
+
+/*
+Final event handlers
+
+onmouseup
+	handles when the mouse is lifted, calling clickup() and
+	allowing any ongoing move operations to finish.
+*/
+document.onmouseup   = function(event){clickup  (event)};
+
+
