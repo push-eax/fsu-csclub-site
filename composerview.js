cd9def76285d3e1703541a3deb0f1f23d1a68a38
Jim Read <jimmydean886@hotmail.com>
2016-12-23 18:21:36 -0500

    Started work on widget theme
    Started themeing widgets loosely based on Adwaita.
    
    If someone in the group doesn't approve of this, too bad because
    this wasn't supposed to be my job in the first place.

--- composerview.js
+++ composerview.js
@@ -2,9 +2,12 @@ function addComposerWindow(){
 	cpsr = addWindow("Composer", 480)
 	composercontents = ""
 	composercontents += "<div class=toolbar>"
-	composercontents += "<button class=tbutton id=save>Save entry</button>"
+	composercontents += "<button class=tbutton id=save>"
+	//composercontents += "<img class=stock src=saveicon.svg></img>"
+	composercontents += "Save entry</button>"
 	composercontents += "</div>"
 	composercontents += "<div class=contentbody>"
+	composercontents += "<textarea class=filltext></textarea>"
 	composercontents += "</div>"
 	setWindowContents(cpsr, composercontents);
 }
