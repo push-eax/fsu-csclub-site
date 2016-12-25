d12c459921d989a6d6c0fd14d8bce0e326e2cac7
Jim Read <jimmydean886@hotmail.com>
2016-12-25 13:27:50 -0500

    More composer view example updates
    Composer view continues to be updated with more example content.
    It has become clear at this point that it is a better idea to use
    a <div contenteditable> tag instead of a plain textedit tag in
    order to make the composer view, because this would allow for a
    little more formatting freedom.

--- composerview.js
+++ composerview.js
@@ -1,3 +1,4 @@
+var bold = false;
 function addComposerWindow(){
 	cpsr = addWindow("Composer", 480)
 	composercontents = ""
@@ -5,9 +6,24 @@ function addComposerWindow(){
 	composercontents += "<button class=tbutton id=save>"
 	//composercontents += "<img class=stock src=saveicon.svg></img>"
 	composercontents += "Save entry</button>"
+	composercontents += "<button class=tbutton id=bold><b>B</b></button>"
+	composercontents += "<button class=tbutton id=italic><i>i</i></button>"
+	composercontents += "<button class=tbutton id=under><u>U</u></button>"
 	composercontents += "</div>"
 	composercontents += "<div class=contentbody>"
-	composercontents += "<textarea class=filltext></textarea>"
+	composercontents += "<div contenteditable class=filltext></div>"
 	composercontents += "</div>"
 	setWindowContents(cpsr, composercontents);
+}
+
+
+function makebold(){
+	if(bold == false){
+		document.getElementById("bold").setAttribute("class", "toggled");
+		bold = true;
+	}
+	else {
+		document.getElementById("bold").setAttribute("class", "tbutton");
+		bold = false;
+	}
 }
