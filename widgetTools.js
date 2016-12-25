ca895bb624b5d570e67544421ac33b3c714353c1
Jim Read <jimmydean886@hotmail.com>
2016-12-25 13:43:16 -0500

    Set up buttons in widget toolkit
    Buttons are now handled by the widget toolkit, meaning that they
    now are properly mangled by various JS functions instead of HTML
    black magic.

--- widgetTools.js
+++ widgetTools.js
@@ -1,4 +1,29 @@
-function makeWidgetSpace(){
+function makeWidgetSpace(parent){
 	var widgetSpace = document.createElement("div");
 	widgetSpace.setAttribute("class", "widgetSpace");
+	parent.appendChild(widgetSpace);
+	return widgetSpace;
 }
+
+function makeButton(parent, type, text){
+	var newButton = document.createElement("button");
+	newButton.setAttribute("class", type);
+	newButton.innerHTML = text;
+	parent.appendChild(newButton);
+	var buttonObject = { button:newButton, type:type, parent:parent, state:"normal" };
+}
+
+function setWidgetText(parent, text){
+	parent.innerHTML = text;
+}
+
+function setButtonToggled(button){
+	button.button.setAttribute("class", "toggled");
+	button.state="toggled";
+}
+
+function setButtonUntoggled(button){
+	button.button.setAttribute("class", button.type);
+	button.state="normal"
+}
+
