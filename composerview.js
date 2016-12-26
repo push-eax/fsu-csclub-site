467e22e37817749d27b376626d8e5ea150090419
Jim Read <jimmydean886@hotmail.com>
2016-12-25 23:50:34 -0500

    Fixed composer window to toggle buttons
    Button toggle now works on B i and U button.

--- composerview.js
+++ composerview.js
@@ -1,13 +1,43 @@
 var bold = false;
 function addComposerWindow(){
-	cpsr = addWindow("Composer", 480)
-	wsp  = makeWidgetSpace()
-	tbr  = makeToolbar(wsp)
+	var cpsr = addWindow("Composer", 480)
+	var wsp  = makeWidgetSpace()
+	var tbr  = makeToolbar(wsp)
 	setWindowSpace(cpsr, wsp);
-	saveButton = makeButton(tbr, "tbutton", "Save");
-	boldButton = makeButton(tbr, "tbutton", "<b>B</b>");
-	italButton = makeButton(tbr, "tbutton", "<i>i</i>");
-	udlnButton = makeButton(tbr, "tbutton", "<u>U</u>");
-	composer   = makeTextArea(wsp);
+	var saveButton = makeButton(tbr, "tbutton", "Save");
+	var boldButton = makeButton(tbr, "tbutton", "<b>B</b>");
+	var italButton = makeButton(tbr, "tbutton", "<i>i</i>");
+	var udlnButton = makeButton(tbr, "tbutton", "<u>U</u>");
+	var composer   = makeTextArea(wsp);
+	setClickAction(boldButton.button, function(){clickBold(boldButton, composer)})
+	setClickAction(italButton.button, function(){clickItal(italButton, composer)})
+	setClickAction(udlnButton.button, function(){clickUdln(udlnButton, composer)})
+}
+
+function clickBold(button, composer){
+	if (button.state == "normal"){
+		setButtonToggled(button)
+	}
+	else {
+		setButtonUntoggled(button)
+	}
+}
+
+function clickItal(button, composer){
+	if (button.state == "normal"){
+		setButtonToggled(button)
+	}
+	else {
+		setButtonUntoggled(button)
+	}
+}
+
+function clickUdln(button, composer){
+	if (button.state == "normal"){
+		setButtonToggled(button)
+	}
+	else {
+		setButtonUntoggled(button)
+	}
 }
 
