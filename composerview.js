cdc9503a4a92705bbac72f217e766f461eff16ca
Jim Read <jimmydean886@hotmail.com>
2016-12-28 20:27:10 -0500

    Made some minor refactoring changes
    Organized some code in the composer dialog script

--- composerview.js
+++ composerview.js
@@ -1,17 +1,17 @@
 var bold = false;
 function addComposerWindow(){
-	var cpsr = addWindow("Composer", 480)
-	var wsp  = makeWidgetSpace()
-	var tbr  = makeToolbar(wsp)
-	setWindowSpace(cpsr, wsp);
+	var cpsr       = addWindow("Composer", 480)
+	var wsp        = makeWidgetSpace()
+	var tbr        = makeToolbar(wsp)
 	var saveButton = makeButton(tbr, "tbutton", "Save");
 	var boldButton = makeButton(tbr, "tbutton", "<b>B</b>");
 	var italButton = makeButton(tbr, "tbutton", "<i>i</i>");
 	var udlnButton = makeButton(tbr, "tbutton", "<u>U</u>");
 	var composer   = makeTextArea(wsp);
-	setClickAction(boldButton.button, function(){clickBold(boldButton, composer)})
-	setClickAction(italButton.button, function(){clickItal(italButton, composer)})
-	setClickAction(udlnButton.button, function(){clickUdln(udlnButton, composer)})
+    setWindowSpace ( cpsr, wsp );
+	setClickAction ( boldButton.button, function() { clickBold( boldButton, composer ) } );
+	setClickAction ( italButton.button, function() { clickItal( italButton, composer ) } );
+	setClickAction ( udlnButton.button, function() { clickUdln( udlnButton, composer ) } );
 }
 
 function clickBold(button, composer){
