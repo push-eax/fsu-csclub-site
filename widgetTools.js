f164594457acdbda88799db4667f421090eaf329
Jim Read <jimmydean886@hotmail.com>
2016-12-29 00:42:18 -0500

    Added POC documentation to widgetTools.js
    widgetTools had no documentation. widgetTools needs documentation.
    Gondor has no king. Gondor needs no king.

--- widgetTools.js
+++ widgetTools.js
@@ -1,9 +1,49 @@
+/*
+widgetTools.js widget toolkit.
+
+This widget toolkit was created with the express purpose of
+making window instantiation smoother for programs which
+require multiple windows of the same type. While by no means
+perfect, widgetTools aims to create a seamless and integrated
+environment which does not toy with higher-level objects in
+the DOM, containing all items in one widget space.
+*/
+
+/*
+makeWidgetSpace()
+
+Creates the toplevel div which is used by default.
+
+Returns:
+	widgetSpace
+		a div DOM object, used as the parent for other
+		/make.+\(\)/ functions such as makeButton() in
+		order to place themselves in one contained
+		environment. Includes the correct class name
+		and other data to fit properly into a window.
+*/
 function makeWidgetSpace(){
 	var widgetSpace = document.createElement("div");
 	widgetSpace.setAttribute("class", "widgetSpace");
 	return widgetSpace;
 }
 
+/*
+makeToolbar(widgetSpace)
+
+Creates a toolbar. Designed to be placed at the top of a
+window. Note that this toolkit intentionally does not mess
+with the idea of a menubar since those actions would produce
+too much unneccesary complexity, at the detriment of ease-
+of-use and bandwidth
+
+Arguments:
+	widgetSpace
+		the DOM element to place the toolbar in.
+		This can conceptually be any DOM element, but
+		it is reccommended that this be restricted
+		to just a widgetSpace.
+*/
 function makeToolbar(widgetSpace){
 	var newToolbar = document.createElement("div");
 	newToolbar.setAttribute("class", "toolbar");
@@ -11,6 +51,30 @@ function makeToolbar(widgetSpace){
 	return newToolbar;
 }
 
+/*
+makeButton(parent, type, text)
+
+Creates a button in the specified parent.
+
+Arguments:
+	parent
+		The dom object or widget space to place the
+		button in
+
+	type
+		the type of button to use
+		Options are currently "button" and "tbutton"
+
+	text
+		the text label to place inside the button
+
+Returns:
+	buttonObject
+		The new button object which was created
+		upon completion of the function. In order to
+		access the DOM object itself, use
+		buttonObject.button
+*/
 function makeButton(parent, type, text){
 	var newButton = document.createElement("button");
 	newButton.setAttribute("class", type);
@@ -20,6 +84,23 @@ function makeButton(parent, type, text){
 	return buttonObject;
 }
 
+/*
+makeTextArea(parent)
+
+Makes a multi-line text editor field. Actually uses a div
+with the contentediable bit turned on. This has the advantage
+of allowing for rather complex formatting and a totally
+WYSIWYG formatter. This is one area where a bona-fide textarea
+falls a little bit short.
+
+Arguments:
+	parent
+		Parent DOM object or widget space.
+
+Returns:
+	newTextArea
+		The new text area div DOM object.
+*/
 function makeTextArea(parent){
 	var newTextArea = document.createElement("div");
 	newTextArea.setAttribute("class", "filltext");
@@ -28,20 +109,67 @@ function makeTextArea(parent){
 	return newTextArea;
 }
 
+/*
+setWidgetText(parent, text)
+
+sets the inner HTML content of an element. Could potentially
+do more in the future, but for now just a basic placeholder.
+
+Arguments:
+	parent
+		The parent DOM object or widget space
+
+	text
+		The text to insert into the HTML
+*/
 function setWidgetText(parent, text){
 	parent.innerHTML = text;
 }
 
+/*
+setButtonToggled(button)
+
+Turns a button into a button that's toggled. In widgetTools
+there is no distinction between a button and a toggle button,
+so this allows the click action of a button to turn it into
+a toggle button
+
+Arguments
+	button
+		the button object to work with
+*/
 function setButtonToggled(button){
 	button.button.setAttribute("class", "toggled");
 	button.state="toggled";
 }
 
+/*
+setButtonUntoggled(button)
+
+Does the reverse of above
+
+Arguments
+	button
+		the button object to work with
+*/
 function setButtonUntoggled(button){
 	button.button.setAttribute("class", button.type);
 	button.state="normal"
 }
 
+/*
+setClickAction(widget, funct)
+
+Sets the click action for an element. Essentially connects the
+onclick property for whatever object you pass it.
+
+Arguments:
+	widget
+		the DOM element to change
+
+	funct
+		the function to connect the click event to
+*/
 function setClickAction(widget, funct){
 	widget.onmousedown = function(){funct()}
 }
