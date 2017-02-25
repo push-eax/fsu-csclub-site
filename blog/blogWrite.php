637e86d6e5252617183d13b1f7366a9fdb6270bc
Chris <chrsitopher.r.odom@gmail.com>
2017-02-24 23:39:16 -0500

    Added script to be used for blog editing

---
+++ blog/blogWrite.php
@@ -0,0 +1,18 @@
+/*
+This script will be used for saving information written 
+in the composer view and turning into a blog.cfg file. This
+blog.cfg file will contain a title, post tile, and a post
+body. 
+
+*/
+
+/*
+TODO 
+    parse string(s) passed from the composer view
+    convert special whitespace chracters and markers
+        (IE Bold Italics and Underline) into escape 
+        sequences
+    dump output into blog.cfg file
+    (optional)
+    think of a way to edit existing blogs
+*/
