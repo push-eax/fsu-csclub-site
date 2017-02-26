f804b98fc523e6c914a7080a5af57c738e0196d9
Chris <chrsitopher.r.odom@gmail.com>
2017-02-26 01:16:55 -0500

    Added function that writes to .ini file

--- resources/Blog.php
+++ resources/Blog.php
@@ -3,7 +3,8 @@
 /*
 This class will handle individual blogposts and organize
 them into a single blog. It does this by handling an array
-of BlogPosts
+of BlogPosts. This is useful for allowing us to easily edit, 
+add, or remove blogpposts once everything is set up. 
 */
 
 require 'BlogPost.php';
@@ -19,8 +20,30 @@ class Blog
 		$this->blogPosts = array();
 	}
 	
+	/*
+	This adds a blogPost object to the array. 
+	*/
+	
 	function addBlogPost($postTitle, $postBody)
 	{
 		$blogPosts[] = new BlogPost(postTitle, postBody);
+	}
+	
+	/*
+	Dumps info into ini file
+	*/
+	function writeToFile()
+	{
+		$filename = "../blog/" . $title . ".ini";
+		$blogFile = fopen($filename);
+		
+		fwrite($blogFile, "[blog]\ntitle = " . $title . "\n";
+		
+		foreach ($blogPosts as $post)
+		{
+			fwrite($blogFile, $post);
+		}
+		
+		fclose($blogFile);
 	}
 }
