31204bd6399b751b0bcb30efbb919df4b37c76a3
Chris <chrsitopher.r.odom@gmail.com>
2017-02-26 00:10:17 -0500

    Implemented basic add functionality to the Blog class

--- resources/Blog.php
+++ resources/Blog.php
@@ -1,1 +1,26 @@
- 
+<?php
+
+/*
+This class will handle individual blogposts and organize
+them into a single blog. It does this by handling an array
+of BlogPosts
+*/
+
+require 'blogger.php';
+
+class Blog
+{
+	public $title
+	public $blogPosts
+	
+	function __construct($titleIn)
+	{
+		$this->title = $titleIn;
+		$this->blogPosts = array();
+	}
+	
+	function addBlogPost($postTitle, $postBody)
+	{
+		$blogPosts[] = new BlogPost(postTitle, postBody);
+	}
+}
