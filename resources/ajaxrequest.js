7e1cc5ba0645a230ac9bcb906f1d2c4ad349d656
Jim Read <jimmydean886@hotmail.com>
2017-04-29 11:57:24 -0400

    Finished blog browser with async AJAX
    the blog browser used to rely on synchronous AJAX to work, however, this
    is deprecated and likely to cause problems. The browser now relies on
    a specific JS backend working properly to display asynchronously.

--- resources/ajaxrequest.js
+++ resources/ajaxrequest.js
@@ -1,17 +1,8 @@
 function getBlogList(){
 	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function(){
-		if(this.readyState == 4 && this.status == 200){
-			//process the request return
-			return JSON.parse(this.responseText);
-		}
-	};
-	xhttp.open("GET", "backend.php?request=searchblog//", false);
+	xhttp.open("GET", "backend.php?request=searchblog//", true);
 	xhttp.send();
-	if(xhttp.readyState == 4 && xhttp.status == 200)
-		return JSON.parse(xhttp.responseText);
-	else
-		return [["1","error"],["2","reload"],["3","page"]];
+	return xhttp
 }
 
 function getBlogById(idnum){
--- resources/blogBrowser.js
+++ resources/blogBrowser.js
@@ -1,13 +1,18 @@
 function makeBrowserWindow(){
-	var bloglist = getBlogList();
-	var toplevel = addWindow("Browse Blog Directory", 600);
-	var wspace = makeWidgetSpace();
-	setWidgetSpace(toplevel, wspace);
-	var tbar = makeToolbar(wspace);
-	var backbutton = makeButton(tbar, "tbutton", "Back");
-	var browsersection = makeSection(wspace);
-	for(var i = 0; i<bloglist.length; i++){
-		makeIcon(browsersection, bloglist[i][1], "folder");
-	}
+	var blogxhttp = getBlogList();
+	blogxhttp.onreadystatechange = function(){
+		if(this.readyState == 4 && this.status==200){
+			bloglist = JSON.parse(this.responseText);
+			var toplevel = addWindow("Browse Blog Directory", 600);
+			var wspace = makeWidgetSpace();
+			setWidgetSpace(toplevel, wspace);
+			var tbar = makeToolbar(wspace);
+			var backbutton = makeButton(tbar, "tbutton", "Back");
+			var browsersection = makeSection(wspace);
+			for(var i = 0; i<bloglist.length; i++){
+				makeIcon(browsersection, bloglist[i][1], "folder");
+			}
+		}
+	};
 }
 addStartupHook(makeBrowserWindow)
