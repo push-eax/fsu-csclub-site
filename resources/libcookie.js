374028e34672b163cbbfdd018d37303fc54dee66
Jim Read <jimmydean886@hotmail.com>
2017-05-11 14:24:47 -0400

    Commented libcookie
    libcookie now contains the same standard comment format as the other
    portions of the interface. It is designed to act as API documentation.
    
    One improvement which will eventually be applied to the rest of the
    comments:
    Data types are specified with arguments and returns in order to avoid
    unexpected exceptions in the code.

--- resources/libcookie.js
+++ resources/libcookie.js
@@ -3,17 +3,39 @@
 
    a simple cookie management library
 
-   templates to start with:
-   	storecookie("key", "value");
-	deletecookie("key");
-	commitcookies();
+   Initially this looked like a much more daunting task, so this library was created. It
+   now serves as a quick way to take care of basic cookie tasks without rewriting code
+   over and over again.
 */
 
+/*
+ * storecookie(key, value)
+ * 
+ * A simple cookie storage function. Takes the key and value, assigns them to the
+ * document.cookie variable with the correct path. Does not store an expiration date.
+ * TODO: add this.
+ * 
+ * Arguments:
+ * 	key (string)
+ * 		The cookie key
+ * 	value (string)
+ * 		the value to store in the cookie
+ */
 function storecookie(key, value){
 	var ck = key+"="+value+";path=/;";
 	document.cookie = ck;
 }
 
+/*
+ * deletecookie(key)
+ * 
+ * Deletes the cookie specified by the string key by setting its expiration date to the
+ * UNIX epoch
+ * 
+ * Arguments:
+ * 	key (string)
+ * 		the key of the cookie to delete
+ */
 function deletecookie(key){
 	var date = new Date();
 	d.setTime(d.getTime() + (daystoexpire*24*60*60*1000));
@@ -21,6 +43,19 @@ function deletecookie(key){
 	document.cookie = key+";"+expires;
 }
 
+/*
+ * grabcookievalue(key)
+ * 
+ * gets the value of the specified cookie.
+ * 
+ * arguments:
+ * 	key (string)
+ * 		the key of the cookie to get.
+ * 
+ * returns:
+ * 	value (string)
+ * 		the value assigned to the key specified.
+ */
 function grabcookievalue(key){
 	var ck = document.cookie;
 	var cks = ck.split(';');
