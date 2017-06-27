79fac73b51df0a29a887102f3a0a527edd306c8b
Jim Read <jimmydean886@hotmail.com>
2017-06-27 13:14:46 -0400

    Started work on making the composer able to also edit things
    The composer (for the moment) is built to compose a blog post, however,
    it sucks at editing anything. This means that it can't effectively act
    as a full solution for working with posts.
    
    This adds some contextual functionality to the composer.
    
    Also in this commit:
    COMMENTS! COMMENTS! COMMENTS!

--- resources/backendinterface.js
+++ resources/backendinterface.js
@@ -12,20 +12,28 @@ It has no returns, no arguments. The rstring variable should be set, and the
 gotauth variable should be set upon success.
 */
 function login(){
+    //We add our login window
     var cpsrwin = addWindow("User Login", 480, "center");
+    //make a widget space
     var widgets = makeWidgetSpace();
-    var toolbar = makeToolbar(widgets);
+    //and then prompt the user for a username and password
     var message = "Please enter your username and password:";
+    //Then we explain who has user access, and what it is for.
     var messagl = makeLabel(widgets, message);
     var descrbx = makeSection(widgets);
     var descrlb = makeLabel(descrbx, "Members of the CS Club who have requested a login can use this function to gain access to their blog(s), and other features of the site. Note that this is for members only, and public accounts are not available at this point in time.");
+    //Then we make a greeter box
     var loginbx = makeSection(widgets);
+    //Username label/field
     var userlbl = makeLabel(loginbx, "<b>Username:</b> ");
     var userbox = makeInput(loginbx, "text", "jonsmith", "user");
+    //password label/field
     var passlbl = makeLabel(loginbx, "<b>Password:</b> ");
     var passbox = makeInput(loginbx, "password", "password", "pass");
+    //and a button next to the password box.
     var loginbt = makeButton(loginbx, "button", "Log In");
     setClickAction(loginbt.button, function(){actlogin(userbox, passbox, cpsrwin);});
+    //Set the widget space and it's off to the races!
     setWidgetSpace(cpsrwin, widgets);
 }
  /*
@@ -43,31 +51,48 @@ Arguments:
                the only component which would use a sign-in.
 */
 function actlogin(userbox, passbox, cpsrwin){
+    //Create the XHTTP request
     var xhttp = new XMLHttpRequest();
+    //And set our header information and query variables.
     xhttp.open("POST", "include/loginsys.cgi", true);
     xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
     xhttp.send("login="+encodeURIComponent(userbox.value)+"&passwd="+encodeURIComponent(passbox.value));
+    //And add a finish handler. The easier-to-read method of doing this is to have the request send synchronously,
+    //but that is slated to be axed, so asynchronous is the way to go.
     xhttp.onreadystatechange = function(){
+        //Assuming we get a correct response (which we should)
         if(this.readyState == 4 && this.status==200){
+            //Parse the response.
             var loginresponse = JSON.parse(this.responseText);
+            //And handle when things go wrong.
             if(loginresponse.Response == "ENOAUTH"){
 	        var dialogwindow = addDialogWindow("Login Failed", 300, "center");
-	        var widgetSpace = makeWidgetSpace();
+	        var widgetSpace  = makeWidgetSpace();
 	        makeLabel(widgetSpace, "Login failed. Please check your username and password, then try again.");
                 setWidgetSpace(dialogwindow, widgetSpace);
-            } else if (loginresponse.Response == "ENOCONNECTION"){
+            }
+            else if (loginresponse.Response == "ENOCONNECTION"){
                 var dialogwindow = addDialogWindow("Login Failed", 300,"center");
-                var widgetSpace = makeWidgetSpace();
+                var widgetSpace  = makeWidgetSpace();
                 makeLabel(widgetSpace, "Login failed due to a database error. Check the site installation or contact an administrator.");
                 setWidgetSpace(dialogwindow, widgetSpace);
-            } else {
+            }
+            //But if we get no error code, we can assume things went right. From here, we set the uname and rstring
+            //variables, and add more actions to the main menu. This will allow people to then 
+            else {
+                //Grab the rstring from the server
                 rstring = loginresponse.response;
+                //the username we sent was good, it should work
                 uname = userbox.value;
+                //close the login window
                 cpsrwin.close();
-                var menutable = document.getElementById("menutable");
-                var composerLine = makeTableRow(menutable);
-                var newpstbutton = makeButton(makeTableData(composerLine), "menubutton", "New Post");
+                //add our menu items, first by grabbing the appropriate table, then by adding table row/data information.
+                var menutable     = document.getElementById("menutable");
+                var composerLine  = makeTableRow(menutable);
+                var newpstbutton  = makeButton(makeTableData(composerLine), "menubutton", "New Post");
                 var editpstbutton = makeButton(makeTableData(composerLine), "menubutton", "Edit Post");
+                setClickAction(newpstbutton.button(),  function(){ addComposerWindow("compose"); });
+                setClickAction(editpstbutton.button(), function(){ /*Not implemented*/});
             }
         }
     }
@@ -83,7 +108,11 @@ The window is designed to act as a place where the user types blog details.
 Upon completion, it should have a "new blog" and "existing blog" mode or similar mechanism
 so that it does not clone blog posts every time an edit is made.
 */
-function addComposerWindow(){
+function addComposerWindow(mode){
+    if(mode != "compose" && mode != "edit"){
+        var errorwindow = addDialogWindow("ERROR", 200, "center");
+        setWindowContents(errorwindow, "The composer has no mode set!");
+    }
     var composerwin = addWindow("Composer Tab Window", 500);
     var widgetSpace = makeWidgetSpace();
     var toolbar     = makeToolbar(widgetSpace);
@@ -108,7 +137,8 @@ function addComposerWindow(){
     setClickAction  ( boldButton.button, function() { clickBold( boldButton, composer ); textSpace.value = composer.innerHTML; } );
     setClickAction  ( italButton.button, function() { clickItal( italButton, composer ); textSpace.value = composer.innerHTML; } );
     setClickAction  ( udlnButton.button, function() { clickUdln( udlnButton, composer ); textSpace.value = composer.innerHTML; } );
-    setClickAction  ( saveButton.button, function() { makePstBd( textSpace.value ); } );
+    if( mode == "compose" ) setClickAction  ( saveButton.button, function() { makePstBd( textSpace.value ); mode = "edit" } );
+    if( mode == "edit" )    setClickAction  ( saveButton.button, function() { setWindowContents(addDialogWindow("Not Implemented", 300, "center"), "Not implemented yet. See GitLab for status."); } )
     setWidgetSpace  (composerwin, widgetSpace);
     return composerwin;
 }
