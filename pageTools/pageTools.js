884a9d705b7c1ca59b9c2d45b0a866b877d4328b
Jim Read <jimmydean886@hotmail.com>
2017-09-03 14:51:39 -0400

    Moved last .js to new code style

--- pageTools/pageTools.js
+++ pageTools/pageTools.js
@@ -2,112 +2,98 @@ var page;
 var topbar;
 var ismobile = true;
 
-/*
-PageTools is designed with a JavaScript-first approach, much unlike windowTools. This
-means it is built to initialize its own elements. If it is defined in the header, it
-cannot do this since the body has not been created yet. initPageTools() creates the
-default body layout.
-*/
-function initPageTools(){
-	//The top bar is kind of hidden away so it does not get affected by rouge programs
-	var realtopbar = document.createElement("div");
-	realtopbar.setAttribute("class", "topbar");
-	document.body.appendChild(realtopbar);
-	
-	//The top bar programs will see is actually a member div of the real topbar, so we
-	//don't override the menu button and logo.
-	topbar = document.createElement("div");
-	topbar.setAttribute("class", "faketbar");
-	realtopbar.appendChild(topbar);
-	
-	//Our site is clearly branded.
-	var logo = document.createElement("img");
-	logo.setAttribute("class", "logo");
-	logo.setAttribute("src", "resources/fsulogo.png");
-	realtopbar.appendChild(logo);
-	
-	//The menu button, which will open a side pane.
-	var menu = document.createElement("img");
-	menu.setAttribute("class", "menubutton");
-	menu.setAttribute("id", "menubutton");
-	menu.setAttribute("src", "pageTools/beta-start-button-3.png");
-	realtopbar.appendChild(menu);
-	
-	//The page is where window elements will be created. This is what widgetSpaces should
-	//be placed into.
-	page = document.createElement("div");
-	page.setAttribute("class", "page");
-	document.body.appendChild(page);
+function initPageTools()
+{
+    var realtopbar = document.createElement("div");
+    realtopbar.setAttribute(
+        "class",
+        "topbar");
+    document.body.appendChild(realtopbar);
+    
+    topbar = document.createElement("div");
+    topbar.setAttribute(
+        "class",
+        "faketbar");
+    realtopbar.appendChild(topbar);
+    
+    var logo = document.createElement("img");
+    logo.setAttribute(
+        "class",
+        "logo");
+    logo.setAttribute(
+        "src",
+        "resources/fsulogo.png");
+    realtopbar.appendChild(logo);
+    
+    var menu = document.createElement("img");
+    menu.setAttribute(
+        "class",
+        "menubutton");
+    menu.setAttribute(
+        "id",
+        "menubutton");
+    menu.setAttribute(
+        "src",
+        "pageTools/beta-start-button-3.png");
+    realtopbar.appendChild(menu);
+    
+    page = document.createElement("div");
+    page.setAttribute(
+        "class",
+        "page");
+    document.body.appendChild(page);
 }
 
-/*
-addWindow(title, width)
+function addWindow(
+    title,
+    width=0)
+{
+    page.innerHTML="";
+    topbar.innerHTML=title;
+    return "page"
+}
 
-Replaces the page with a new one
-
-Arguments:
-	title
-		Top bar title
-	
-	width
-		kept for compatibility with windowTools
-
-returns
-	"page"
-*/
-function addWindow(title, width=0){
+function closeWindow(window)
+{
+    if(window == "page")
+    {
 	page.innerHTML="";
-	topbar.innerHTML=title;
-	return "page"
+    }
+    else
+    {
+	delete window;
+    }
 }
 
-/*
-closeWindow(window)
-
-Resets the page or closes the element.
-
-Arguments:
-	window
-		"page" if it's the main page, otherwise deletes the object.
-*/
-function closeWindow(window){
-	if(window == "page"){
-		page.innerHTML="";
-	}
-	else{
-		delete window;
-	}
+function addDialogWindow(
+    title="Blergh",
+    width=0)
+{
+    var dialog = document.createElement("div");
+    dialog.setAttribute(
+        "class",
+        "dialog");
+    setTimeout(
+        function()
+        {
+            document.body.removeChild(dialog)
+        },
+        10000);
+    document.body.appendChild(dialog);
+    return dialog;
 }
 
-/*
-addDialogWindow(title, width)
-
-Adds a dialog bar to the bottom
-
-Arguments:
-	title
-		Unused.
-	
-	width
-		like title, kept for compatibility. See addWindow()
-
-returns:
-	a dialog div.
-*/
-function addDialogWindow(title="Blergh", width=0){
-	var dialog = document.createElement("div");
-	dialog.setAttribute("class", "dialog");
-	setTimeout(function(){document.body.removeChild(dialog)}, 10000);
-	document.body.appendChild(dialog);
-	return dialog;
-}
-
-function setWidgetSpace(window, wspace){
-	if(window == "page"){
-		page.innerHTML="";
-		page.appendChild(wspace);
-	}
-	else{
-		window.appendChild(wspace);
-	}
+function setWidgetSpace(
+    window,
+    wspace)
+{
+    if(window == "page")
+    {
+	page.innerHTML="";
+	page.appendChild(wspace);
+    }
+    else
+    {
+	window.appendChild(wspace);
+    }
 }
